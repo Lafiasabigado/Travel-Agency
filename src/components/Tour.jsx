69f1f60adfaad4281bfa05ee2a34ffda94223d96
Lafiasabigado <lafiasabigado97@gmail.com>
2025-01-14 15:17:51 +0100

    tour package coding

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -9,14 +9,20 @@ const Tour = () => {
       ]
     
       return (
-        <div className='mx-0 px-0'>
+        <>
+        <div className='mx-0 px-0 pt-4'>
             <h2 className='font-bold text-2xl text-center'>The <span className='text-orange-400'>best place</span> for vacation</h2>
-            <div className='flex flex-wrap justify-center items-center'>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-4 p-4'>
                 {places.map((place,index) => (
-                   <div className='bg-white p-4 rounded-lg shadow-md text-center flex-1 min-w-[150px] max-w-[200px]' key={index}>{place.label}</div>
+                   <div className='py-2 px-1 bg-white hover:bg-orange-300 shadow-md border-zinc-400 rounded-md text-center' key={index}>{place.label}</div>
                 ))}
             </div>
         </div>
+        <div className=''>
+
+        </div>
+        </>
+        
       )
 }
 

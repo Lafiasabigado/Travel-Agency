82a6a8d091b139ca2ef77849fa31242235162ea5
Lafiasabigado <lafiasabigado97@gmail.com>
2025-01-16 16:16:27 +0100

    tour coding

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaArrowRight, FaMapMarkerAlt, FaStar } from 'react-icons/fa'; // Import des icônes
+import { FaArrowLeft } from 'react-icons/fa6';
+
 
 const Tour = () => {
     const places = [
@@ -6,7 +9,90 @@ const Tour = () => {
         {label:"Populer"},
         {label:"Recommendation"},
         {label:"Best Price"},
-      ]
+    ]
+    
+    const tours = [
+      {
+        id: 1,
+        imageUrl: "/packimg1.png",
+        location: "Maldives",
+        rating: 4.7,
+        title: "Hurawalhi Island",
+        duration: "7 Days",
+        people: 2,
+        price: 620,
+      },
+      {
+        id: 2,
+        imageUrl: "/packimg2.png",
+        location: "Indonesia",
+        rating: 4.5,
+        title: "Bali Province",
+        duration: "4 Days",
+        people: 2,
+        price: 780,
+      },
+      {
+        id: 3,
+        imageUrl: "/packimg3.png",
+        location: "Spain",
+        rating: 4.7,
+        title: "Barcelona city beach",
+        duration: "4 Days",
+        people: 4,
+        price: 850,
+      },
+      {
+        id: 4,
+        imageUrl: "/packimg4.png",
+        location: "Maldives",
+        rating: 4.7,
+        title: "Hurawalhi Island",
+        duration: "7 Days",
+        people: 2,
+        price: 620,
+      },
+      {
+        id: 5,
+        imageUrl: "/packimg5.png",
+        location: "Canadana",
+        rating: 4.7,
+        title: "St. John's",
+        duration: "7 Days",
+        people: 2,
+        price: 620,
+      },
+      {
+        id: 6,
+        imageUrl: "/packimg6.png",
+        location: "Maldives",
+        rating: 4.7,
+        title: "Machu Picchu",
+        duration: "5 Days",
+        people: 4,
+        price: 820,
+      },
+      {
+        id: 7,
+        imageUrl: "/packimg7.png",
+        location: "French",
+        rating: 4.7,
+        title: "Bora Bora Island",
+        duration: "7 Days",
+        people: 2,
+        price: 550,
+      },
+      {
+        id: 8,
+        imageUrl: "/packimg8.png",
+        location: "Australia",
+        rating: 4.5,
+        title: "Sydney Opera House",
+        duration: "7 Days",
+        people: 2,
+        price: 310,
+      },
+    ] 
     
       return (
         <>
@@ -18,8 +104,41 @@ const Tour = () => {
                 ))}
             </div>
         </div>
-        <div className=''>
-
+        <div className='grid lg:grid-cols-4 md:grid-cols-2 m-2 p-2 gap-4'>
+           {tours.map((tour) => (
+            <div key={tour.id} className='flex-col rounded-md shadow-md'>
+            <img src={tour.imageUrl} alt="" className='object-cover w-full' />
+            <div className='flex py-2 m-1'>
+              <div className='flex-1 text-orange-400'>
+                <div className='flex justify-start items-center'>
+                  <FaMapMarkerAlt className=''/>
+                  <span className='px-2'>{tour.location}</span>
+                </div>
+              </div>
+              <div className='flex-2 text-orange-400'>
+                <div className='flex justify-end items-center'>
+                  <FaStar className='text-yellow-400'/>
+                  <span className='px-2'>{tour.rating}</span>
+                </div>
+              </div>
+            </div>
+            <p className='font-bold text-xl m-1'>{tour.title}</p>
+            <p className='text-gray-400 m-1'>{tour.duration} Tour on {tour.people} person</p>
+            <div className='flex py-2 m-1'>
+              <div className='flex-1 text-orange-400'>
+                <div className='flex justify-start'>
+                  <span className='px-2  font-bold text-xl'>{tour.price} $</span>
+                </div>
+              </div>
+              <div className='flex-2 text-gray-400'>
+                <div className='flex justify-end items-center'>
+                  <span className='px-2'>View more</span>
+                  <FaArrowRight />
+                </div>
+              </div>
+            </div>
+          </div>
+          ))}
         </div>
         </>
         

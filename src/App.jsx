54488949d2d622355bf294014f709fbb4e39ab56
Lafiasabigado <lafiasabigado97@gmail.com>
2025-01-17 17:58:50 +0100

    code review

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter as Router , Routes, Route , Link  } from 'react-router-do
 import  Header from './components/Header'
 import  About from './components/About'
 import  Home from './components/Home'
+import Tour from './components/Tour'
 
 const App = () => {
   return (
     <Router>
       <Link to='/'></Link>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/header' element={<Header></Header>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/header' element={<Header />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/tour' element={<Tour />}></Route>
       </Routes>
     </Router>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
             <Link to="/about">About Us</Link>
           </li>
           <li className="flex items-center text-lg hover:text-orange-500 px-4 py-2 lg:px-0">
-            <Link to="/todo">Tour Packages</Link>
+            <Link to="/tour">Tour Packages</Link>
           </li>
           <li className="flex items-center text-lg hover:text-orange-500 px-4 py-2 lg:px-0">
             <Link to="/contact">Our Services</Link>

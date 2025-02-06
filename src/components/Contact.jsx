253896bfff700effffa235d89878026a0f396fd4
Lafiasabigado <lafiasabigado97@gmail.com>
2025-02-06 14:39:13 +0100

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,10 +32,10 @@ const Contact = () => {
   return (
     <div className='m-2 pt-10'>
         <div className='m-4'>
-            <h2 className='text-center font-semibold text-2xl mx-2 my-2'>
+            <h2 className='font-semibold text-2xl mx-2 my-2'>
               Contact us to review <span className='text-orange-400'>your experience </span> with us
             </h2>
-            <p className='text-center text-gray-400 mx-2 my-2'>
+            <p className='text-gray-400 mx-2 my-2'>
               Give us feedback and let us know what experiences you
               had while on vacation with us
             </p>
